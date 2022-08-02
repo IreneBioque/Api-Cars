c8195d6b5f85c9f0ece0b10e8e043282c66258a7
Irene Bioque <irenebioque@gmail.com>
2022-08-02 18:09:13 +0200

    add validatios Pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // solo deja la data que yo estoy esperando
+      whitelist: true,
+      // regresa un error 400 cuando mandas una data que no esta esperando
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
